0d86778b17b247fea8fb52a6e3344c5597e2adb4
Elliott Thompson <ellth92@gmail.com>
2019-11-04 19:49:55 +0000

    added extra updates

--- app/index.jsx
+++ app/index.jsx
@@ -1,65 +1,67 @@
-import { render, h, Component } from 'preact';
-import React from 'react';
-import { Provider } from 'react-redux';
-import PropTypes from 'prop-types';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import 'font-awesome/css/font-awesome.css';
-import styled from 'styled-components';
+import { render, h, Component } from "preact";
+import React from "react";
+import { Provider } from "react-redux";
+import PropTypes from "prop-types";
+import { HashRouter as Router, Route } from "react-router-dom";
+import "font-awesome/css/font-awesome.css";
+import styled from "styled-components";
 
-import store from 'store';
-import { Wrapper }  from 'styles';
-import Background from 'components/background';
-import Homepage from 'components/homepage';
-import Contact from 'components/contact';
+import store from "store";
+import { Wrapper } from "styles";
+import Background from "components/background";
+import AnimatedBox from "components/animatedBox";
+import Homepage from "components/homepage";
+import Contact from "components/contact";
 
-import scrollEvent from 'scrollEvent';
-import 'preact/devtools';
+import scrollEvent from "scrollEvent";
+// import "preact/devtools";
 
 /*
  * Global Config
  */
 const CONFIG = {
-    SHOW_BLOG: false
+  SHOW_BLOG: false
 };
 
 function mapStateToProps(state) {
-    return {
-        nearBottomOfPage: state.scroll.nearBottomOfPage
-    };
-};
+  return {
+    nearBottomOfPage: state.scroll.nearBottomOfPage
+  };
+}
 
 class App extends Component {
-    getChildContext() {
-        return {
-            CONFIG
-        };
-    }
-    render() {
-        return (
-            <Router>
-                <Wrapper height='100%'>
-                    <Background/>
-                    <Wrapper height='100%'>
-                        <Homepage/>
-                    </Wrapper>
-                    <Wrapper id='contact' height='100%'>
-                        <Contact/>
-                    </Wrapper>
-                </Wrapper>
-            </Router>
-        );
-    }
+  getChildContext() {
+    return {
+      CONFIG
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Wrapper height="100%">
+          <Background />
+          <AnimatedBox />
+          <Wrapper height="100%">
+            <Homepage />
+          </Wrapper>
+          <Wrapper id="contact" height="100%">
+            <Contact />
+          </Wrapper>
+        </Wrapper>
+      </Router>
+    );
+  }
 }
 
 App.childContextTypes = {
-      CONFIG: PropTypes.object
+  CONFIG: PropTypes.object
 };
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // register global events
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,59 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ENV = process.env.NODE_ENV || 'production';
-
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ENV = process.env.NODE_ENV || "production";
 
 module.exports = {
-    context: path.resolve(__dirname, './app'),
-    entry: {
-        app: './index.jsx',
+  context: path.resolve(__dirname, "./app"),
+  entry: {
+    app: "./index.jsx"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js"
+  },
+  devtool: ENV === "development" ? "eval-source-map" : false,
+  resolve: {
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat"
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    devtool: (ENV === 'development') ? 'eval-source-map' : false,
-    resolve: {
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat'
-        },
-        modules: [
-            path.join(__dirname, './app'),
-            "node_modules"
-        ],
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                include: path.resolve(__dirname, './app'),
-                exclude: [/node_modules/],
-                use: [{
-                    loader: 'babel-loader',
-                    options: { presets: ['es2015', 'react'] }
-                }],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
-                loader: 'file-loader',
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-            }
+    modules: [path.join(__dirname, "./app"), "node_modules"],
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, "./app"),
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["es2015", "react"] }
+          }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('bundle.css'),
-        new HtmlWebpackPlugin({
-            title: 'Elliott Thompson',
-            favicon: 'favicon.jpg',
-            template: 'index.ejs'
+      },
+      {
+        test: /\.(png|eot|svg|ttf|woff|woff2|otf)$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
         })
+      }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+    new HtmlWebpackPlugin({
+      title: "Elliott Thompson",
+      favicon: "favicon.jpg",
+      template: "index.ejs"
+    })
+  ]
 };
